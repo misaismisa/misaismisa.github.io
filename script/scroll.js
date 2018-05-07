e7d5d86780370db38523bfe447fc932c26f6ff51
Misa Nguyen <misanguyen1390@gmail.com>
2018-05-07 12:33:27 -0700

    updated project file

--- script/scroll.js
+++ script/scroll.js
@@ -1,13 +1,20 @@
-// ===== Scroll to Top ====
-$(window).scroll(function() {
-    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
-        $('#return-to-top').fadeIn(200);    // Fade in the arrow
-    } else {
-        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
-    }
+jQuery(document).ready(function() {
+  var offset = 250;
+  var duration = 300;
+  jQuery(window).scroll(function() {
+  if (jQuery(this).scrollTop() > offset) {
+  jQuery(‘.back-to-top’).fadeIn(duration);
+  } else {
+  jQuery(‘.back-to-top’).fadeOut(duration);
+  }
+
 });
-$('#return-to-top').click(function() {      // When arrow is clicked
-    $('body,html').animate({
-        scrollTop : 0                       // Scroll to top of body
-    }, 500);
+
+
+jQuery(‘.back-to-top’).click(function(event) {
+  event.preventDefault();
+  jQuery(‘html, body’).animate({scrollTop: 0}, duration);
+  return false;
+})
+
 });
