afa1a17d5b9255530f818e1743195aa48194b527
Misa Nguyen <misanguyen1390@gmail.com>
2018-08-20 23:31:28 -0700

    style edits to homepage

--- script/scroll.js
+++ script/scroll.js
@@ -18,6 +18,8 @@ $('#toggle').click(function() {
 $(document).ready(function(){
   $('a[href*="#"]').on('click', function(event){
 
+    // $('#overlay').toggleClass('close');
+    // $(this).toggleClass('active');
     // event.preventDefault();
 
     var hash = this.hash;
@@ -30,3 +32,43 @@ $(document).ready(function(){
     });
   });
 });
+// $(document).ready(function(){
+// var Vue = require('vue');
+// var VueScrollTo = require('vue-scrollto');
+//
+// Vue.use(VueScrollTo)
+//
+// // You can also pass in the default options
+// VueScrollTo.setDefaults({
+//     container: "body",
+//     duration: 500,
+//     easing: "ease",
+//     offset: 0,
+//     cancelable: true,
+//     onStart: false,
+//     onDone: false,
+//     onCancel: false,
+//     x: false,
+//     y: true
+// })
+//  })
+//  });
+
+$(document).ready(function() {
+
+    var wHeight = $(window).height();
+
+    $('.slide')
+      .height(wHeight)
+      .scrollie({
+        scrollOffset : -50,
+        scrollingInView : function(elem) {
+
+          var bgColor = elem.data('background');
+
+          $('body').css('background-color', bgColor);
+
+        }
+      });
+
+  });
